31797180551dcec211b93e9f1babf2b8207f1f83
yyyhd <113404871+yyyhd@users.noreply.github.com>
2025-05-06 15:30:50 +0800

    SSIM.py

--- Evaluation/SSIM.py
+++ Evaluation/SSIM.py
@@ -4,8 +4,8 @@ import numpy as np
 from scipy import signal
 from enum import Enum
 
-real_folder = r"D:\code\cem1\RISS1\buchong\Evaluation\real"
-fake_folder = r"D:\code\cem1\RISS1\buchong\Evaluation\syn"
+real_folder = ""Evaluation/real""
+fake_folder = ""Evaluation/syn"
 
 
 def filter2(img, fltr, mode='same'):
